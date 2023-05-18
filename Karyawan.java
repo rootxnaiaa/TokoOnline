bc502580a51d47e4a1cb52ef83dc924b9a14525c
Naila Rizki <114964873+rootxnaiaa@users.noreply.github.com>
2023-05-18 21:10:21 +0700

    Update Karyawan.java

--- Karyawan.java
+++ Karyawan.java
@@ -1,49 +1,65 @@
-package toko.online;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package toko_online;
 import java.util.ArrayList;
-
-public class Karyawan implements User {
+/**
+ *
+ * @author Naila Rizki
+ */
+public class karyawan implements user {
     private ArrayList<String> namaKaryawan = new ArrayList<String>();
     private ArrayList<String> alamat = new ArrayList<String>();
     private ArrayList<String> telepon = new ArrayList<String>();
     private ArrayList<Integer> jabatan = new ArrayList<Integer>();
 
-    public Karyawan() {
-        // constructor
+    public karyawan() {
+        this.namaKaryawan.add("admin");
+        this.alamat.add("Tulungagung");
+        this.telepon.add("085768890121");
+        this.jabatan.add(0);
     }
 
-    public void setJabatan(int index, int value) {
-        jabatan.set(index, value);
+    public void setJabatan(int jabatan) {
+        this.jabatan.add(jabatan);
     }
 
-    public int getJabatan(int index) {
-        return jabatan.get(index);
+    public int getJabatan(int id) {
+        return this.jabatan.get(id);
     }
 
     public int getJmlKaryawan() {
-        return namaKaryawan.size();
+        return this.namaKaryawan.size();
     }
 
+    @Override
     public void setNama(String nama) {
-        namaKaryawan.add(nama);
+        this.namaKaryawan.add(nama);
     }
 
+    @Override
     public void setAlamat(String alamat) {
         this.alamat.add(alamat);
     }
 
+    @Override
     public void setTelepon(String telepon) {
-        this.telepon.add(telepon);
+        this.telepon.add("089891123654");
+    }
+    @Override
+    public String getNama(int id) {
+        return this.namaKaryawan.get(id);
     }
 
-    public String getNama(int index) {
-        return namaKaryawan.get(index);
+    @Override
+    public String getTelepon(int id) {
+        return this.telepon.get(id);
     }
 
-    public String getAlamat(int index) {
-        return alamat.get(index);
+    @Override
+    public String getAlamat(int id) {
+        return this.alamat.get(id);
     }
-
-    public String getTelepon(int index) {
-        return telepon.get(index);
-    }
+    
 }
