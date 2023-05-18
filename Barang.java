54e846c5c5a0d838f7b30c451511a911201f003f
Naila Rizki <114964873+rootxnaiaa@users.noreply.github.com>
2023-05-18 21:07:40 +0700

    barang.java

--- Barang.java
+++ Barang.java
@@ -1,67 +1,61 @@
-package toko.online;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package toko_online;
 import java.util.ArrayList;
-
-public class Barang implements User {
+/**
+ *
+ * @author Naila Rizki
+ */
+public class barang {
     private ArrayList<String> namaBarang = new ArrayList<String>();
     private ArrayList<Integer> stok = new ArrayList<Integer>();
     private ArrayList<Integer> harga = new ArrayList<Integer>();
+    public barang(){
+        this.namaBarang.add("Chocolate");
+        this.stok.add(15);
+        this.harga.add(10000);
 
-    public Barang() {}
+        this.namaBarang.add("Bodywash");
+        this.stok.add(30);
+        this.harga.add(5000);
 
-    public void setNamaBarang(String nama) {
-        this.namaBarang.add(nama);
+        this.namaBarang.add("Mouse");
+        this.stok.add(20);
+        this.harga.add(15000);
     }
 
-    public String getNamaBarang(int index) {
-        return this.namaBarang.get(index);
-    }
-
-    public int getJmlBarang() {
+    public int getJmlgBrg() {
         return this.namaBarang.size();
     }
 
-    public void setStok(int stok) {
+    public void setNamaBarang(String namaBarang) {
+        this.namaBarang.add(namaBarang);
+    }
+
+    public String getNamaBarang(int idBarang) {
+        return this.namaBarang.get(idBarang);
+    }
+
+    public void setStok(Integer stok) {
         this.stok.add(stok);
     }
 
-    public int getStok(int index) {
-        return this.stok.get(index);
+    public int getStok(int idBarang) {
+        return this.stok.get(idBarang);
     }
 
-    public void editStok(int index, int stok) {
-        int currentStok = this.stok.get(index);
-        this.stok.set(index, currentStok + stok);
+    public void editStok(int stok, int idBarang) {
+        this.stok.set(idBarang,stok);
     }
 
     public void setHarga(int harga) {
         this.harga.add(harga);
     }
 
-    public int getHarga(int index) {
-        return this.harga.get(index);
+    public int getHarga(int idBarang) {
+        return this.harga.get(idBarang);
     }
-
-    @Override
-    public void setNama(String nama) {}
-
-    @Override
-    public void setAlamat(String alamat) {}
-
-    @Override
-    public void setTelepon(String telepon) {}
-
-    @Override
-    public String getNama(int index) {
-        return this.namaBarang.get(index);
-    }
-
-    @Override
-    public String getAlamat(int index) {
-        return null;
-    }
-
-    @Override
-    public String getTelepon(int index) {
-        return null;
-    }
+    
 }
